28a4e3f85a79d642290bd8bed95b768238f3a14a
ON-DELL <ON-DELL@ON-DELL-PC>
2019-03-22 22:33:02 +0700

    test script 12

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>TEST DEPLOY SCRIPT 11</h1>
+        <h1>TEST DEPLOY SCRIPT 12</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
