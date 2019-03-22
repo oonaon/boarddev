6a1aa6de7e88d697ce0f25ccbf49cc90081126d2
ON-DELL <ON-DELL@ON-DELL-PC>
2019-03-22 21:15:30 +0700

    test auto deploy 1

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>HELLO WORLD</h1>
+        <h1>AUTO DEPLOY 1</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
